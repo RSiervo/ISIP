59087f92e6b3e562c5b97687da845386d13aaccb
RSiervo <rostomsiervo14@gmail.com>
2026-01-15 10:59:18 +0800

    fix chunk limit

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,17 @@ export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': process.env
-  }
+  },
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 });
